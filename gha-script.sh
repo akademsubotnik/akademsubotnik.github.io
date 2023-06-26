f1e7f751afd2822c944c4b8424e7aa8b17f13a46
Greg <44036625+akademsubotnik@users.noreply.github.com>
2023-06-26 23:22:11 +0300

    Update gha-script.sh

--- gha-script.sh
+++ gha-script.sh
@@ -1,4 +1,11 @@
-echo What is in this directory?
+echo 1 - What is in this directory?
 ls -a
-echo What is the current directory?
+echo
+echo 2 - What is the current directory?
 pwd
+echo
+echo 3 - How is the disk laid out?
+df -h
+echo
+echo 4 - What are the environment variables?
+env
